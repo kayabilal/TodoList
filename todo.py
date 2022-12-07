25ac0911f6d05ed66ef54ddd3bc13797aa49da36
bilalkaya123 <84505170+bilalkaya123@users.noreply.github.com>
2022-12-07 14:53:29 +0300

    some codes are added.

--- todo.py
+++ todo.py
@@ -20,7 +20,7 @@ class Todo(db.Model):
 def index(): 
     todos = Todo.query.all()
     return render_template("index.html",todos=todos)
-#asdasasdasdsad
+
 #complete
 @app.route("/complete/<string:id>")
 def completeTodo(id):
@@ -53,6 +53,27 @@ def deleteTodo(id):
     db.session.commit()
     return redirect(url_for("index"))
 
+"""@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home"))"""
 
 if __name__ == "__main__":
     with app.app_context():
